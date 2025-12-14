f46935d38aa72258f1995d25b8892197cd83e5de
Salis02 <nizarsalis@gmail.com>
2025-12-14 21:29:49 +0700

    feat: update index.js api, export both category and review api

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,7 @@ import apiClient from './client';
 
 export { authApi } from './auth.api';
 export { productApi } from './product.api';
+export { categoryApi } from './category.api'
+export { reviewApi } from './review.api'
 
 export default apiClient;

3c40a2bb9451397456bdbe1b16b0a1a84b3a3d2a
Salis02 <nizarsalis@gmail.com>
2025-12-19 22:10:07 +0700

    feat: add toaster for best UI/UX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import { AuthProvider } from "./context/AuthContext"
 import { CartProvider } from "./context/CartContext"
 import { AppRoutes } from "./routes/AppRoutes"
+import { Toaster } from 'react-hot-toast'
 
 export default function App() {
   return (
     <AuthProvider>
       <CartProvider>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 3000,
+            style: {
+              background: '#333',
+              color: '#fff'
+            },
+            success: {
+              duration: 3000,
+              iconTheme: {
+                primary: '#10b981',
+                secondary: '#fff',
+              },
+            },
+            error: {
+              duration: 4000,
+              iconTheme: {
+                primary: '#ef4444',
+                secondary: '#fff',
+              },
+            },
+          }}
+        />
         <AppRoutes />
       </CartProvider>
     </AuthProvider>

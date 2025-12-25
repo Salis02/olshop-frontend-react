e7b2f27686f4b0bde61562b80b7f4932f441c68f
Salis02 <nizarsalis@gmail.com>
2025-12-25 22:51:47 +0700

    feat: wishlist api has been added in index.js api

--- src/api/index.js
+++ src/api/index.js
@@ -5,5 +5,6 @@ export { productApi } from './product.api';
 export { categoryApi } from './category.api'
 export { reviewApi } from './review.api'
 export { cartApi } from './cart.api';
+export { wishlistApi } from './wishlist.api';
 
 export default apiClient;
